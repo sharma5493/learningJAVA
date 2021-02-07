c25756b840421953c792be8ac032969e0a958d54
sachin <sachinhp445@gmail.com>
2021-02-07 21:01:08 +0530

    second commit

---
+++ src/JUSTtOcHECKgIT.java
@@ -0,0 +1,13 @@
+
+public class JUSTtOcHECKgIT  {
+
+	public static void main(String[] args) {
+	    int x=5;
+		
+	    while (x<5)
+	    {
+	    	System.out.println("no is less than 5");
+	    }
+	}
+
+}
